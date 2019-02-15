38823a55d84358012f22454abd19998f7503e2e8
Antonín Kříž <15214494+antoninkriz@users.noreply.github.com>
2019-02-15 09:30:08 +0100

    Updated parser to use UUID, added file Data.sql with all data to be imported in the DB

--- Tools/KarolinkaToSQL/main.py
+++ Tools/KarolinkaToSQL/main.py
@@ -1,36 +1,64 @@
+from uuid import *
 from urllib import request
 from lxml.html import parse, fromstring
 from lxml.etree import tostring
 
 obory = []
 predmety = {}
+katedry = {}
 
+docHtml = str(request.urlopen('http://studium.pedf.cuni.cz/karolinka/2018/plany.html').read(), 'utf-8').split(
+    'CŽV Studium pedagogických věd')[0]
+prezencniHtml, kombinovaneHtml = docHtml.split('Kombinované studium', 1)
+prezBak, prezencniHtml = prezencniHtml.split('Bakalářské studium', 1)
+prezMag, prezNav = prezencniHtml.split('Navazující magisterské studium', 1)
+kombBak, kombinovaneHtml = kombinovaneHtml.split('Bakalářské studium', 1)
+kombMag, kombNav = kombinovaneHtml.split('Navazující magisterské studium', 1)
+druhy = {
+    'bak': {
+        'data': prezBak + kombBak,
+        'nazev': 'Bakalářské',
+        'zkratka': 'Bc.',
+        'id': uuid4()
+    },
+    'mag': {
+        'data': prezMag + kombMag,
+        'nazev': 'Magisterské',
+        'zkratka': 'Mgr.',
+        'id': uuid4()
+    }, 'nav': {
+        'data': prezNav + kombNav,
+        'nazev': 'Navazující magisterské',
+        'zkratka': 'NMgr.',
+        'id': uuid4()
+    }
+}
 
 idObory = 1
-docHtml = str(request.urlopen('http://studium.pedf.cuni.cz/karolinka/2018/plany.html').read(), 'utf-8').split('CŽV Studium pedagogických věd')[0]
-doc = fromstring(docHtml)
-for table in doc.cssselect('table'):
-    for row in table.cssselect('tr:not(:first-child)'):
-        tds = row.cssselect('td')
+for d in druhy:
+    doc = fromstring(druhy[d]['data'])
+    for table in doc.cssselect('table'):
+        for row in table.cssselect('tr:not(:first-child)'):
+            tds = row.cssselect('td')
 
-        kod = tds[0].text_content()
-        obor = tds[1].text_content()
-        specifikace = tds[2].text_content()
-        platnostOd = tds[3].text_content()
-        platnostDo = tds[4].text_content()
+            kod = tds[0].text_content()
+            obor = tds[1].text_content()
+            specifikace = tds[2].text_content()
+            platnostOd = tds[3].text_content()
+            platnostDo = tds[4].text_content()
 
-        obory.append({
-            'id': idObory,
-            'kod': None if kod == '' else kod,
-            'obor': None if obor == '' else obor,
-            'specifikace': None if specifikace == '' else specifikace,
-            'platnostOd': None if platnostOd == '' else platnostOd,
-            'platnostDo': None if platnostDo == '' else platnostDo,
-            'predmety': []
-        })
+            obory.append({
+                'id': uuid4(),
+                'druh': druhy[d]['id'],
+                'kod': None if kod == '' else kod,
+                'obor': None if obor == '' else obor,
+                'specifikace': None if specifikace == '' else specifikace,
+                'platnostOd': None if platnostOd == '' else platnostOd,
+                'platnostDo': None if platnostDo == '' else platnostDo,
+                'predmety': []
+            })
 
-        idObory += 1
-
+            idObory += 1
 
 for o in obory:
     doc = parse('http://studium.pedf.cuni.cz/karolinka/2018/' + o['kod'] + '.html').getroot()
@@ -82,12 +110,11 @@ for o in obory:
                 'ukKLP': ukKLP
             })
 
-
 idPredmety = 1
 for o in obory:
     for p in o['predmety']:
         if p['kod'] not in predmety:
-            p['id'] = idPredmety
+            p['id'] = uuid4()
             predmety[p['kod']] = {
                 'obory': [o['id']],
                 'predmet': p
@@ -97,40 +124,61 @@ for o in obory:
         else:
             predmety[p['kod']]['obory'].append(o['id'])
 
+        if p['katedra'] not in katedry:
+            uid = uuid4()
+            katedry[p['katedra']] = uid
+            p['katedra'] = uid
+        else:
+            uid = katedry[p['katedra']]
+            p[id] = uid
 
-out = 'INSERT INTO tblObory VALUES '
+out = 'INSERT INTO tblDruhyStudia VALUES '
+for d in druhy:
+    out += '({}, {}, {}),\n'.format(
+        'UNHEX("' + str(druhy[d]['id']).replace('-', '') + '")',
+        '"' + druhy[d]['zkratka'] + '"',
+        '"' + druhy[d]['nazev'] + '"')
+out = out[:-2] + ';\n'
+
+out += 'INSERT INTO tblObory VALUES '
 for o in obory:
-    out += '({}, {}, {}, {}, {}, {}),'.format(o['id'],
-                                              '"' + o['kod'] + '"' if o['kod'] else 'NULL',
-                                              '"' + o['obor'] + '"' if o['obor'] else 'NULL',
-                                              '"' + o['specifikace'] + '"' if o['specifikace'] else 'NULL',
-                                              o['platnostOd'] if o['platnostOd'] else 'NULL',
-                                              o['platnostDo'] if o['platnostDo'] else 'NULL')
-out = out[:-1] + ';'
-
+    out += '({}, UNHEX("{}"), {}, {}, {}, {}, {}),\n'.format('UNHEX("' + str(o['id']).replace('-', '') + '")',
+                                                             str(o['druh']).replace('-', ''),
+                                                             '"' + o['kod'] + '"',
+                                                             '"' + o['obor'] + '"',
+                                                             '"' + o['specifikace'] + '"' if o[
+                                                                 'specifikace'] else 'NULL',
+                                                             o['platnostOd'] if o['platnostOd'] else 'NULL',
+                                                             o['platnostDo'] if o['platnostDo'] else 'NULL')
+out = out[:-2] + ';\n'
 
 out += 'INSERT INTO tblPredmety VALUES '
 for pr in predmety:
     p = predmety[pr]['predmet']
-    out += '({}, {}, {}, {}, {}, {}, {}, {}, {}),'.format(p['id'],
-                                                          '"' + p['kod'] + '"' if p['kod'] else 'NULL',
-                                                          '"' + p['nazev'] + '"' if p['nazev'] else 'NULL',
-                                                          '"' + p['kredity'] + '"' if p['kredity'] else 'NULL',
-                                                          '"' + p['katedra'] + '"' if p['katedra'] else 'NULL',
-                                                          1 if p['ukZ'] else 0,
-                                                          1 if p['ukKZ'] else 0,
-                                                          1 if p['ukZK'] else 0,
-                                                          1 if p['ukKLP'] else 0)
-out = out[:-1] + ';'
+    out += '({}, {}, {}, {}, {}, {}, {}, {}, {}),\n'.format('UNHEX("' + str(p['id']).replace('-', '') + '")',
+                                                            '"' + p['kod'] + '"',
+                                                            '"' + p['nazev'] + '"',
+                                                            p['kredity'],
+                                                            'UNHEX("' + str(p['katedra']).replace('-', '') + '")',
+                                                            1 if p['ukZ'] else 0,
+                                                            1 if p['ukKZ'] else 0,
+                                                            1 if p['ukZK'] else 0,
+                                                            1 if p['ukKLP'] else 0)
+out = out[:-2] + ';\n'
 
+out += "INSERT INTO tblKatedry VALUES "
+for k in katedry:
+    out += '({}, {}),\n'.format('UNHEX("' + str(katedry[k]).replace('-', '') + '"), ',
+                                k)
+out = out[:-2] + ';\n'
 
 out += 'INSERT INTO tblPredmetyNaObory VALUES '
-idPredmetyNaObory = 1
 for o in obory:
     for pr in o['predmety']:
-        out += '({}, {}, {}),'.format(idPredmetyNaObory, o['id'], predmety[pr['kod']]['predmet']['id'])
-        idPredmetyNaObory += 1
-out = out[:-1] + ';'
-
+        out += '(UNHEX("{}"), UNHEX("{}"), UNHEX("{}")),\n'.format(str(uuid4()).replace('-', ''),
+                                                                   str(o['id']).replace('-', ''),
+                                                                   str(predmety[pr['kod']]['predmet']['id']).replace(
+                                                                       '-', ''))
+out = out[:-2] + ';\n'
 
 print(out)
